5d20f1fc5b8a523f0e0fd3c848c53f2f67107f81
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2022-11-11 13:19:41 -0300

    Criação da função installDependentPackages para instalar pacotes auxiliares da configuração do projeto

--- script.sh
+++ script.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 updateServer(){
-    #
-    echo Atualizando repositório...
+    # Atualizando lista de pacotes
+    echo Atualizando lista de pacotes...
     apt-get -qq update
 
-    #
+    # Atualizando pacotes
     echo Atualizando pacotes...
     apt-get -qq upgrade
 }
 
 installApache2(){
-    #
-    echo Instalando Apache2
+    # Instalando servidor web Apache2
+    echo Instalando Apache2...
     apt-get -qq install apache2
+}
+
+installDependentPackages(){
+    # Instalando pacotes dependentes para configuração do projeto
+    echo Instalando dependências...
+    apt-get -qq install unzip wget
 }
