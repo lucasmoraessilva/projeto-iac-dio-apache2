b107a5943c361d3698b092fb1e887f8468737d22
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2022-11-11 22:41:22 -0300

    Criação da função transferFilesToApacheFolder para descompactar arquivos do site e movê-los para o diretório de exibição do Apache2

--- script.sh
+++ script.sh
@@ -27,3 +27,13 @@ downloadSitesFiles(){
     echo "Baixando arquivos do site..."
     wget -q -P /tmp https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 }
+
+transferFilesToApacheFolder(){
+    # Descompatando arquivos do site
+    echo "Descompatando arquivos do site..."
+    unzip -qq /tmp/main.zip -d /tmp
+
+    # Movendo arquivos para o diretório de exibição do site
+    echo "Movendo arquivos do site para o diretório de exibição..."
+    mv /tmp/linux-site-dio-main/* /var/www/html/
+}
